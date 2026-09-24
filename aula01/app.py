26ab10b17eb0f1ee07af99534d0274cefd16960d
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2026-09-23 21:48:17 -0300

    leitura de dados e inclusão das metricas

--- aula01/app.py
+++ aula01/app.py
@@ -2,6 +2,25 @@
 """
 
 import streamlit as st
+import dados
 
+st.set_page_config(layout="wide")
 st.title("📚 Dashboard de Livros")
 st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
+
+col1, col2, col3 = st.columns(3)
+
+livros = dados.ler_livros()
+
+qtd_livros = len(livros)
+
+col1.metric("Total de livros:" , qtd_livros)
+
+preco_medio = dados.calculo_preco_medio(livros)
+
+col2.metric("Preço médio:" , f"£{preco_medio:.2f}")
+
+cinco_estrelas = dados.contar_cinco_estrelas(livros)
+col3.metric("Qtd de livros com 5 estrelas : " ,  cinco_estrelas)
+st.dataframe(livros)
+
